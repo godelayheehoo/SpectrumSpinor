9ed14270a186d975c8e9b507e920034c1c1f7b74
James <shawjr3@gmail.com>
2025-11-05 09:30:09 -0500

    noop for menu in subcal

--- src/MenuManager.cpp
+++ src/MenuManager.cpp
@@ -865,7 +865,7 @@ void MenuManager::octaveMenuBackButton(){
 void MenuManager::SharedCalibrationMenuRender(int selectedIdx, int scrollIdx){
     display.clearDisplay();
     display.setTextSize(1);
-    const char* menus[13] = {"Dark Offset", "Gains", "LightBlue","Orange", "Pink", "Yellow", "Green", "Red", "Black","DarkBlue", "Purple", "White", "Reset Defaults"};
+    const char* menus[13] = {"No-Op""Dark Offset", "Gains", "LightBlue","Orange", "Pink", "Yellow", "Green", "Red", "Black","DarkBlue", "Purple", "White", "Reset Defaults"};
     int yStart = 10;
     display.setCursor(10,yStart);
     display.setTextColor(OLED_WHITE);
